ae041e678ede0cc70c21e1462a3e7e9067d8a8b0
root <root@localhost>
2016-09-23 14:59:33 +0800

    Something changed.

--- Server-Side/main.cpp
+++ Server-Side/main.cpp
@@ -5,22 +5,32 @@
 #include <vector>
 #include <mutex>
 
-#include "../SmallWorld20160916/MapBox.h"
+#include "../MapBox.h"
 using namespace std;
 #define MAXWIDTH 120
 #define MAXHEIGHT 40
 
+inline bool inround(int LeftBound,int& val,int RightBound)
+{
+	return (LeftBound<=val&&val<=RightBound);
+}
+
+#define MAX_UNIT_TYPE 128
+int _unit_basic_dmg[MAX_UNIT_TYPE];
+int _unit_max_hp[MAX_UNIT_TYPE];
+string _unit_default_name[MAX_UNIT_TYPE];
+
 int getUnitBasicDamage(int UnitType)
 {
-
+	return _unit_basic_dmg[UnitType];
 }
 int getUnitMaxHP(int UnitType)
 {
-
+	return _unit_max_hp[UnitType];
 }
 string getUnitDefaultName(int UnitType)
 {
-
+	return _unit_default_name[UnitType];
 }
 
 struct userinfo
@@ -55,7 +65,7 @@ struct cityinfo
 
 unitinfo getUintByID(int id)
 {
-
+	
 }
 
 
@@ -67,21 +77,21 @@ cityinfo getCityByID(int id)
 struct blockinfo
 {
     int id;
-    int party;/// 0:Empty 1:City-Union 2:Independent-Party 3:Non-Government
-    int uid;/// UID of owner. 0: Natural
-    int unitid;/// 0: Non-Unit Else: Unit ID
-    int cityid;/// 0: Non-City Else: City ID
+    int party;/// -1: Unknown 0:Empty 1:City-Union 2:Independent-Party 3:Non-Government
+    int uid;/// -1: Unknown .UID of owner. 0: Natural(Empty)
+    int unitid;/// -1: Unknown 0: Non-Unit Else: Unit ID
+    int cityid;/// -1: Unknown 0: Non-City Else: City ID
 };
 
 mutex lock_worldmap;
 blockinfo worldmap[MAXWIDTH][MAXHEIGHT];
 
 /// Positions are sure to be in range.
-vector<vector<blockinfo>> getMapInfo_Vec(int uid,int LeftUpBlockX,int LeftUpBlockY,int RightDownBlockX,int RightDownBlockY)
+vector<vector<blockinfo>> getMapInfo_Vec(int LeftUpBlockX,int LeftUpBlockY,int RightDownBlockX,int RightDownBlockY)
 {
     lock_worldmap.lock();
-    int w=RightDownBlockX-LeftUpBlockX;
-    int h=RightDownBlockY-LeftUpBlockY;
+    int w=RightDownBlockX-LeftUpBlockX+1;
+    int h=RightDownBlockY-LeftUpBlockY+1;
     vector<vector<blockinfo>> vec;
     for(int i=0;i<h;i++)
     {
@@ -102,10 +112,31 @@ class MapInfoClass
 public:
     MapBox toMapBox()
     {
-
+    	MapBox box;
+    	box.add("Content-Type","MapInfo");
+    	box.add("Content-From","Server");
+    	char buff[16];
+    	memset(buff,0,16);
+    	sprintf(buff,"%d",LX);
+    	box.add("LX",buff);
+    	
+    	memset(buff,0,16);
+    	sprintf(buff,"%d",LY);
+    	box.add("LY",buff);
+    	
+    	memset(buff,0,16);
+    	sprintf(buff,"%d",RX);
+    	box.add("RX",buff);
+    	
+    	memset(buff,0,16);
+    	sprintf(buff,"%d",RY);
+    	box.add("RY",buff);
+    	
+    	
     }
 public:
     vector<vector<blockinfo>> vec;
+    int LX,LY,RX,RY;
 };
 
 MapInfoClass getMapInfo(int uid,int LX,int LY,int RX,int RY)
@@ -121,12 +152,68 @@ MapInfoClass getMapInfo(int uid,int LX,int LY,int RX,int RY)
     RightDownY=min(RightDownY,MAXHEIGHT-1);
 
     MapInfoClass cc;
-    cc.vec=getMapInfo_Vec(uid,LeftUpX,LeftUpY,RightDownX,RightDownY);
+    cc.vec=getMapInfo_Vec(LeftUpX,LeftUpY,RightDownX,RightDownY);
+    cc.LX=LeftUpX;
+    cc.LY=LeftUpY;
+    cc.RX=RightDownX;
+    cc.RY=RightDownY;
+    int sz=(cc.RX-cc.LX+1)*(cc.RY-cc.LY+1);
+    char* kk=new char[sz];
+    memset(kk,0,sz);
+    for(int i=0;i<cc.RY-cc.LY+1;i++)
+    {
+    	for(int j=0;j<cc.RX-cc.LX+1;j++)
+    	{
+    		if(cc.vec.at(i).at(j).uid==uid|| (getUserByID(uid).party==cc.vec.at(i).at(j).party && cc.vec.at(i).at(j).party!=3) )
+    		kk[i*(cc.RX-cc.LX+1)+j]=1;
+    		else
+    		kk[i*(cc.RX-cc.LX+1)+j]=0;
+    	}
+    }
+    for(int i=0;i<cc.RY-cc.LY+1;i++)
+    {
+    	for(int j=0;j<cc.RX-cc.LX+1;j++)
+    	{
+    		bool canbevis=kk[i*(cc.RX-cc.LX+1)+j];
+    		//UP
+    		if(!canbevis&&i>=1)
+    		{
+    			if(kk[(i-1)*(cc.RX-cc.LX+1)+j])
+    			canbevis=true;
+    		}
+    		//Down
+    		if(!canbevis&&i<=MAXHEIGHT-2)
+    		{
+    			if(kk[(i+1)*(cc.RX-cc.LX+1)+j])
+    			canbevis=true;
+    		}
+    		//Left
+    		if(!canbevis&&j>=1)
+    		{
+    			if(kk[i*(cc*RX-cc.LX+1)+(j-1)])
+    			canbevis=true;
+    		}
+    		//Right
+    		if(!canbevis&&j<=MAXWIDTH-2)
+    		{
+    			if(kk[i*(cc.RX-cc.LX+1)+(j+1)])
+    			canbevis=true;
+    		}
+    		
+    		if(!canbevis)
+    		{
+    			//cannot be vis
+    			cc.vec.at(i).at(j).party=-1;
+    			cc.vec.at(i).at(j).uid=-1;
+    			cc.vec.at(i).at(j).unitid=-1;
+    			cc.vec.at(i).at(j).cityid=-1;
+    		}
+    	}
+    }
+    
+    
+    
 
     return cc;
 }
 
-int main()
-{
-    return 0;
-}
